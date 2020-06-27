92abd433db9ac02f211dac277057d6f51c41c7b2
JHopeCollins <joshua.hope-collins@eng.ox.ac.uk>
2020-06-27 15:02:32 +0100

    initial testing with mdarray

--- cpp/inc/mdarray/mdarray.h
+++ cpp/inc/mdarray/mdarray.h
@@ -25,20 +25,32 @@
    template<int nDim>
    struct Offset
   {
-      const std::array<size_t,nDim> off;
-      const size_t& operator[]( const int i ) const { return off[i]; }
+      const std::array<int,nDim> offsets;
+      const int& operator[]( const int i ) const { return offsets[i]; }
   };
 
 
 /*
  * return index in position 'offset' relative to 'idx'
+ *    Offload to helper function with integer pack to access idx & off elements
+ *       Idx is a const class, so cannot create new Idx object then add offset
+ *       Cannot hard-code brace initialisation because do not know nDim a-priori
  */
-   template<int nDim>
+   template<int nDim, typename Indices=std::make_index_sequence<nDim>>
    Idx<nDim> operator+( const Idx<nDim> idx, const Offset<nDim> off )
   {
-      Idx<nDim> result(idx);
-      for( int i=0; i<nDim; i++ ){ result[i]+=off[i]; }
-      return result;
+      return idx_add_offset( idx, off, Indices{} );
+  }
+
+/*
+ * helper function to implement addition of Idx and Offset
+ *    uses integer sequence to brace-initialise new Idx. This avoids having to construct
+ */
+   template<int nDim, int... Is>
+      requires (sizeof...(Is)==nDim)
+   Idx<nDim> idx_add_offset( const Idx<nDim> idx, const Offset<nDim> off, std::index_sequence<Is...> )
+  {
+      return {(idx[Is]+off[Is])...};
   }
 
 
@@ -54,30 +66,49 @@
 
 
 /*
- * data type to hold the strides in memory needed to flatten an n-dimensional array
+ * helper function to calculate the strides through a flattened array corresponding to elements of a multidimensional array with shape dims
+ */
+   template<int nDim>
+   std::array<size_t,nDim> make_strides( const Dims<nDim>& dims )
+  {
+      std::array<size_t,nDim> strides;
+      strides[nDim-1]=1;
+      for( int i=nDim-2; i>=0; i-- )
+     {
+         strides[i]=strides[i+1]*dims[i+1];
+     }
+      return strides;
+  }
+
+/*
+ * return length of flattened array corresponding to multi-dimensional array with dimensions dim
+ */
+   template<int nDim>
+   size_t length( const Dims<nDim>& dims )
+  {
+      size_t l=1;
+      for( int i=0; i<nDim; i++ ){ l*=dims[i]; }
+      return l;
+  }
+
+/*
+ * data type to hold the strides in memory needed to flatten an n-dimensional array with no padding
  *    last index changes fastest (row-major)
  *    eg in 3D, the element in memory corresponding to index i,j,k is: stride[0]*i + stride[1]*j + stride[2]*k
  */
    template<int nDim>
    struct Stride
   {
-      const std::array<size_t,nDim> stride;
+      const std::array<size_t,nDim> strides;
 
-      Stride( const Dims<nDim>& dims )
-     {
-         stride[nDim-1]=1;
-         for( int i=nDim-2; i>=0; i-- )
-        {
-            stride[i]=stride[i+1]*dims[i+1];
-        }
-     }
+      Stride( const Dims<nDim>& dims ) : strides(make_strides(dims)) {}
 
-      const size_t& operator[]( const int i ) const { return stride[i]; }
+      const size_t& operator[]( const int i ) const { return strides[i]; }
   };
 
 
 /*
- * calculate the index in the flattened array corresponding n-dimensional index 'idx', in an n-dimensional array with shape 'stride'
+ * calculate the index in the flattened array corresponding to the n-dimensional index 'idx', in an n-dimensional array with shape 'stride'
  */
    template<int nDim>
    size_t operator*( const Stride<nDim>& stride, const Idx<nDim>& idx )
@@ -93,37 +124,39 @@
  *    Has 'nDim' dimensions, and elements of type 'ElemT'
  *    The shape (length in each dimension) must be specified at construction e.g.:
  *       MDArray<double,2> myArray(5,25);
- *    Elements are accessed using an Idx type of the same dimension, e.g.:
+ *    Elements are accessed using an Idx type of the same dimension, which can be constructed in-place e.g.:
  *       Idx<2> idx{2,3};
- *       auto x = myArray[ idx ];
- *       auto y = myArray[ {4,11} ];
+ *       auto x = myArray[ idx ];      // use existing Idx instance
+ *       auto y = myArray[ {4,11} ];   // construct temporary in-place
  */
    template<typename ElemT, int nDim>
    struct MDArray
   {
    // shape of multi-dimensional array
-      Dims<  nDim>   dims;
+      const Dims<nDim>   dims;
 
    // place-values for flattening a multi-dimensional index to a 1D index
-      Stride<nDim> stride;
+      const Stride<nDim> stride;
 
    // the flattened 1D array in memory
       std::vector<ElemT> elems;
 
-   // constructors (no blank construction or copying, only move)
+   // constructors (no construction without dimensions, no copying, only move)
       MDArray() = delete;
-      MDArray( const MDArray&  ) = delete; /**/
+      MDArray( const MDArray&  ) = delete;
       MDArray(       MDArray&& ) = default;
 
    // assignment (no copying, only move)
-      MDArray& operator=( const MDArray&  ) = delete; /**/
+      MDArray& operator=( const MDArray&  ) = delete;
       MDArray& operator=(       MDArray&& ) = default;
 
-   // construct with specified dimensions, calculate place-value strides, and resize flattened array
+   // construct with specified dimensions, calculate place-value strides, and initialise flattened array to correct size
       template<typename... Ints>
          requires   (sizeof...(Ints)==nDim)
                  && (is_integer_v<Ints> && ...)
-      MDArray( Ints... Is ) : dims{Is...}, stride({Is...}), elems( (Is*...) ) {}
+      MDArray( const Ints... Is ) : dims{Is...}, stride({Is...}), elems( (Is*...) ) {}
+
+      MDArray( const Dims<nDim>& d ) : dims(d), stride(d), elems( length(dims) ) {}
 
    // accessors
       const ElemT& operator[]( const Idx<nDim>& idx ) const { return elems[ stride*idx ]; }
--- cpp/script/testArray.cpp
+++ cpp/script/testArray.cpp
@@ -1,7 +1,170 @@
 
 # include <mdarray/mdarray.h>
 
+# include <iostream>
+# include <iomanip>
+
+   template<size_t Nnodes, typename ElemT, typename Functor, typename... ElemTs>
+   void for_each_stencil( const std::array<Offset<3>,Nnodes> stencil,
+                          const Functor                      functor,
+                                MDArray<ElemT, 3>&              array,
+                                MDArray<ElemTs,3>&...           arrays );
+
    int main()
   {
+//    Idx<1> idx1{2};
+//    Idx<2> idx2{3,4};
+
+//    std::cout << idx1[0] << std::endl;
+//    std::cout << idx2[0] << " " << idx2[1] << std::endl;
+
+//    Offset<1> off1{2};
+//    Offset<2> off2{21,13};
+
+//    std::cout << (idx1+off1)[0] << std::endl;
+
+//    Idx idx2p(idx2+off2);
+//    std::cout << idx2p[0] << " " << idx2p[1] << std::endl;
+
+//    std::cout << std::endl; // --------------------------------
+
+      Dims<3> dims3{4,3,4};
+
+//    Stride<3> stride3(dims3);
+
+//    std::cout << stride3[0] << " "
+//              << stride3[1] << " "
+//              << stride3[2] << std::endl;
+
+//    Idx<3> idx3{1,2,3};
+
+//    std::cout << stride3*idx3 << std::endl;
+
+//    std::cout << std::endl; // --------------------------------
+
+      MDArray<double,3> array3(dims3);
+
+      for( size_t i=0; i<dims3[0]; i++ )
+     {
+         for( size_t j=0; j<dims3[1]; j++ )
+        {
+            for( size_t k=0; k<dims3[2]; k++ )
+           {
+               array3[{i,j,k}] = 100*i + 10*j + k;
+           }
+        }
+     }
+
+      for( size_t i=0; i<dims3[0]; i++ )
+     {
+         for( size_t j=0; j<dims3[1]; j++ )
+        {
+            for( size_t k=0; k<dims3[2]; k++ )
+           {
+               std::cout << std::setw(4);
+               std::cout << array3[{i,j,k}] << " ";
+           }
+            std::cout << std::endl;
+        }
+         std::cout << std::endl;
+         std::cout << std::endl;
+     }
+
+      std::cout << std::endl; // --------------------------------
+
+      Offset<3> here{  0, 0, 0};
+      Offset<3> right{ 0, 0, 1};
+      Offset<3> left{  0, 0,-1};
+      Offset<3> front{ 0, 1, 0};
+      Offset<3> back{  0,-1, 0};
+      Offset<3> above{ 1, 0, 0};
+      Offset<3> below{-1, 0, 0};
+
+//    for( size_t i=0; i<dims3[0]; i++ )
+//   {
+//       for( size_t k=1; k<dims3[2]-1; k++ )
+//      {
+//          Idx<3> idx{i,1,k};
+
+//          std::cout << "     " << std::setw(4) << array3[idx+above] << std::endl;
+//          std::cout            << std::setw(4) << array3[idx+ left] << " "
+//                               << std::setw(4) << array3[idx      ] << " "
+//                               << std::setw(4) << array3[idx+right] << std::endl;
+//          std::cout << "     " << std::setw(4) << array3[idx+below] << std::endl;
+//          std::cout << std::endl;
+//      }
+//       std::cout << std::endl;
+//   }
+
+      const auto print_stencil = []( const std::array<double,7>& elems )->void
+     {
+         std::cout << std::setw(4) << elems[0] << std::endl;
+         std::cout << std::setw(4) << elems[1] << " " << std::setw(4) << elems[2] << std::endl;
+         std::cout << std::setw(4) << elems[3] << " " << std::setw(4) << elems[4] << std::endl;
+         std::cout << std::setw(4) << elems[5] << " " << std::setw(4) << elems[6] << std::endl;
+         std::cout << std::endl;
+         return;
+     };
+
+      const auto print_stencil2 = [print_stencil]( const std::array<double,7> elems0, const std::array<double,7> elems1 )->void
+     {
+         print_stencil( elems0 );
+         std::cout << std::endl;
+         print_stencil( elems1 );
+         return;
+     };
+
+      for_each_stencil<7>( {here,left,right,
+                                 back,front,
+                                 below,above},
+                           print_stencil2,
+                           array3,
+                           array3 );
+
       return 0;
   }
+
+   template<size_t Nnodes, int nDim, typename ElemT,
+            typename Stencil_Indices=std::make_index_sequence<Nnodes>>
+   std::array<ElemT,Nnodes> get_stencil_elems(        MDArray<ElemT,nDim>&               array,
+                                               const Idx<nDim>                         centre,
+                                               const std::array<Offset<nDim>,Nnodes>& stencil )
+  {
+      return get_stencil_elems( array, centre, stencil, Stencil_Indices{} );
+  }
+
+   template<size_t Nnodes, int nDim, typename ElemT,
+            size_t... Stencil_Is>
+   std::array<ElemT,Nnodes> get_stencil_elems(       MDArray<ElemT,nDim>&               array,
+                                               const Idx<nDim>                         centre,
+                                               const std::array<Offset<nDim>,Nnodes>& stencil,
+                                               const std::index_sequence<Stencil_Is...> )
+  {
+      return { (array[ centre + stencil[Stencil_Is]])... };
+  }
+
+   template<size_t Nnodes, typename ElemT, typename Functor, typename... ElemTs>
+   void for_each_stencil( const std::array<Offset<3>,Nnodes> stencil,
+                          const Functor                      functor,
+                                MDArray<ElemT, 3>&             array,
+                                MDArray<ElemTs,3>&...          arrays )
+  {
+      for( size_t i=1; i<array.dims[0]-1; i++ )
+     {
+         for( size_t j=1; j<array.dims[1]-1; j++ )
+        {
+            for( size_t k=1; k<array.dims[2]-1; k++ )
+           {
+               Idx<3> idx{i,j,k};
+
+               functor( get_stencil_elems( array,  idx, stencil ),
+                        get_stencil_elems( arrays, idx, stencil )... );
+           }
+        }
+     }
+      return;
+  }
+
+
+
+
